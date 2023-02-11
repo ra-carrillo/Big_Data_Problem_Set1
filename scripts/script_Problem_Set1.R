ad1089d9b3621aee5c006bee64f4d8fbb25bbd5a
Andres Carrillo <racarrillo.research@gmail.com>
2023-02-11 12:55:57 -0500

    Update script_Problem_Set1.R

--- scripts/script_Problem_Set1.R
+++ scripts/script_Problem_Set1.R
@@ -27,7 +27,6 @@
 
   rm(list=ls())
 
-
 #---Configuración inicial : Instalar y llamar paquetes 
   
   install.packages("pacman") # activar solo una vez en el pc
@@ -49,21 +48,21 @@
   
   
   # Aqui vamos a leer el html de la página web donde estan los datos
-  data_html<-read_html("https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_page_1.html")
+  data1_html<-read_html("https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_page_1.html")
   
   # Ver la clase de objeto
-  class(data_html)
-  View(data_html)
+  class(data1_html)
+  View(data1_html)
 
   #Extraer tablas
-  data_html%>%
+  data1_html%>%
   html_table()
   
   # Mostrar el número de tablas extraidas
-  length(data_html)
+  length(data1_html)
   
   # Guardarla como data.frame
-  data<-data_html%>%
+  data1<-data1_html%>%
     html_table()%>%
     as.data.frame()
   
@@ -71,7 +70,7 @@
   # encuentran los datos de la GEIH 2018 para Bogotá
   
   url<-"https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_page_"
-  geih2018<-data.frame()
+  data<-data.frame()
   
   #Loop
   for (i in 1:10){
@@ -79,11 +78,11 @@
     tablas<-url_i%>%
       read_html()%>%
       html_table() %>% .[[1]]
-    geih2018<-rbind.data.frame(geih2018,tablas)
+    data<-rbind.data.frame(data,tablas)
   }
   
   # Eliminar primera columna de la data
-  db<-data[,-1]
+  data<-data[,-1]
   
   # Conocer la ruta del directorio de trabajo
   getwd()
@@ -92,16 +91,16 @@
   setwd("C:/Users/andre/OneDrive/Github/Repositorios/Big_Data_Problem_Set1/data")
   
   #Guardar data
-  write.table(db, "db_geih2018.txt", sep = "\t", quote = F, row.names = F)
+  write.table(data, "db_geih2018.txt", sep = "\t", quote = F, row.names = F)
   
   
 #---Limpieza de la base de datos 
   
-  # Aqui vamos a visualizar una parte de la base de datos
-  db
+  # Visualización de una parte de la base de datos
+  data
   
-  # Ahora vamos a conocer la dimension de la base de datos
-  dim(db)
+  # Dimensión de la base de datos
+  dim(data)
   
   # Podemos ver que tenemos una matriz de 32177 (filas) x 177 (columnas), es decir,
   # tenemos 32.177observaciones y 177 variables
@@ -110,23 +109,23 @@
   # filas de la base de datos
   
   #Primeras filas
-  head(db)
+  head(data)
   
   #Últimas filas
-  tail(db)
+  tail(data)
   
   #Vamos a explorar el tipo de variable de la base de datos
-  glimpse(db)
+  glimpse(data)
   
   #Vamos a mirar si hay valores perdidos
-  is.na(db_geih2018)
+  is.na(data)
   
   #Queremos conocer cuantos valores perdidos hay 
-  sum(is.na(db))
+  sum(is.na(data))
   # En nuestra base de datos hay 3.421.720
   
   # Vamos a seleccionar las variables de interes 
-  geih18<-db %>%
+  geih18<-data %>%
       select(y_salary_m_hu,age,sex)
   
   #Valores perdido en variables especificas 
