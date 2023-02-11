77e89a546d53dac75815c25c56940ca5aadcc279
Andres Carrillo <racarrillo.research@gmail.com>
2023-02-11 12:22:55 -0500

    Update script_Problem_Set1.R

--- scripts/script_Problem_Set1.R
+++ scripts/script_Problem_Set1.R
@@ -23,44 +23,55 @@
 
 # --------------------------------------------------------------------------------
 
-#---Configuración inicial
+#---Limpiar el espacio de trabajo 
 
-#install.packages("pacman") # activar solo una vez en el pc
-library(pacman)
+  rm(list=ls())
 
-## llamar la librería pacman: contiene la función p_load()
-require(pacman)
-require(dplyr)
 
-## El comadno "p_load" permite instalar/cargar las librerías que se enlistan:
-p_load(tidyverse, # contiene las librerías ggplot, dplyr...
-       rvest, # web-scraping
-       stargazer)
+#---Configuración inicial : Instalar y llamar paquetes 
+  
+  install.packages("pacman") # activar solo una vez en el pc
+  library(pacman)
+
+  ## llamar la librería pacman: contiene la función p_load()
+  require(pacman)
+
+  ## El comandoo "p_load" permite instalar/cargar las librerías que se enlistan:
+  p_load(tidyverse, # contiene las librerías ggplot, dplyr...
+         rvest, # web-scraping
+         stargazer,)
 
 #---Paso 1. Descargar base de datos de la GEIH 2018-Bogotá uasndo web-scraping
+  
+  # *Nota: Aqui vamos a realizar el codigo para hacer web-scraping para una sola pag, 
+  # posteriormente haremos un loop para el caso especifico del problem set que hay 
+  # que extraer los datos de varias paginas 
+  
+  
   # Aqui vamos a leer el html de la página web donde estan los datos
-  geih2018_html<-read_html("https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_page_1.html")
+  data_html<-read_html("https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_page_1.html")
   
   # Ver la clase de objeto
-  class(geih2018_html)
-  View(geih2018_html)
+  class(data_html)
+  View(data_html)
 
   #Extraer tablas
-  geih2018_html%>%
+  data_html%>%
   html_table()
   
   # Mostrar el número de tablas extraidas
-  length(geih2018_html)
+  length(data_html)
   
   # Guardarla como data.frame
-  geih2018<-geih2018_html%>%
+  data<-data_html%>%
     html_table()%>%
     as.data.frame()
   
-  # Vamos hacer un loop que replique el paso anterior para las 10 pag
+  # Vamos hacer un loop que replique el paso anterior para las 10 pag donde se 
+  # encuentran los datos de la GEIH 2018 para Bogotá
   
   url<-"https://ignaciomsarmiento.github.io/GEIH2018_sample/pages/geih_page_"
-  data<-data.frame()
+  geih2018<-data.frame()
   
   #Loop
   for (i in 1:10){
@@ -68,7 +79,7 @@ p_load(tidyverse, # contiene las librerías ggplot, dplyr...
     tablas<-url_i%>%
       read_html()%>%
       html_table() %>% .[[1]]
-      data<-rbind.data.frame(data,tablas)
+    geih2018<-rbind.data.frame(geih2018,tablas)
   }
   
   # Eliminar primera columna de la data
